974bc6f347ffda1a356d299759ea990540f7ae55
Omer <62906173+l0rdyolo@users.noreply.github.com>
2025-01-21 01:38:46 +0300

    centered image

--- src/blocks.ts
+++ src/blocks.ts
@@ -23,7 +23,66 @@ export default function(editor: Editor, opts: Required<PluginOptions>) {
     cellStyleStr += `${prop}: ${cellStyle[prop]}; `;
   }
 
-
+  // Register custom image component
+  editor.DomComponents.addType('custom-image', {
+    model: {
+      defaults: {
+        tagName: 'div',
+        droppable: false,
+        attributes: { class: 'image-container' },
+        style: {
+          position: 'relative',
+          width: '100vw',
+          height: 'auto',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          margin: '0',
+          padding: '0',
+          backgroundColor: 'red'
+        },
+        components: [{
+            type: 'image',
+            style: {
+                position: 'relative',
+                maxWidth: '100%',
+                maxHeight: '100%',
+                left: '50%',
+                transform: 'translateX(-50%)',
+                objectFit: 'contain',
+                margin: '0',
+                padding: '0'
+            },
+            attributes: {   
+              src: 'https://picsum.photos/200/300'   
+            }
+        }],
+        traits: [
+          {
+            type: 'button',
+            text: 'Select Image',
+            command: (editor: Editor) => {
+              const selected = editor.getSelected();
+              editor.runCommand('open-assets', {
+                target: selected?.find('img')[0],
+                types: ['image'],
+                accept: 'image/*'
+              });
+            }
+          }
+        ]
+      },
+      init() {
+        this.on('dblclick', () => {
+          editor.runCommand('open-assets', {
+            target: this.find('img')[0],
+            types: ['image'],
+            accept: 'image/*'
+          });
+        });
+      }
+    }
+  });
 
   addBlock('button', {
     label: 'Button',
@@ -33,17 +92,11 @@ export default function(editor: Editor, opts: Required<PluginOptions>) {
     content: '<a class="button">Button</a>',
   });
 
-
-  addBlock('image', {
-    label: 'Image',
+  editor.BlockManager.add('custom-image', {
+    label: 'Centered Image',
     media: `<svg viewBox="0 0 24 24">
-      <path fill="currentColor" d="M21,3H3C2,3 1,4 1,5V19A2,2 0 0,0 3,21H21C22,21 23,20 23,19V5C23,4 22,3 21,3M5,17L8.5,12.5L11,15.5L14.5,11L19,17H5Z" />
+        <path fill="currentColor" d="M21,3H3C2,3 1,4 1,5V19A2,2 0 0,0 3,21H21C22,21 23,20 23,19V5C23,4 22,3 21,3M5,17L8.5,12.5L11,15.5L14.5,11L19,17H5Z" />
     </svg>`,
-    activate: true,
-    content: {
-      type:'image',
-      style: { color:'black' },
-    },
+    content: { type: 'custom-image' }
   });
-
 };
--- src/styles.ts
+++ src/styles.ts
@@ -2,139 +2,103 @@ import type { Editor } from 'grapesjs';
 import { PluginOptions } from '.';
 
 export default function(editor: Editor, opts: Required<PluginOptions>) {
-    // Remove all sectors first
-    editor.StyleManager.getSectors().reset();
+    editor.on('load', () => {
+        // Remove all sectors first
+        editor.StyleManager.getSectors().reset();
 
-    // if (opts.updateStyleManager) {
-    //     const styleManagerSectors = [{
-    //         name: 'Dimension',
-    //         open: false,
-    //         buildProps: ['width', 'height', 'margin', 'padding'],
-    //         properties:[{
-    //           property: 'width',
-    //           units: ['vw'],
-    //           defaults: '10vw',
-    //         },{
-    //           property: 'height',
-    //           units: ['vh'],
-    //           defaults: '10vh',
-    //         },{
-    //           property: 'margin',
-    //           properties:[
-    //             { name: 'Top', property: 'margin-top', units: ['vh'], defaults: '0vh'},
-    //             { name: 'Left', property: 'margin-left', units: ['vw'], defaults: '0vw'},
-    //             { name: 'Right', property: 'margin-right', units: ['vw'], defaults: '0vw'},
-    //             { name: 'Bottom', property: 'margin-bottom', units: ['vh'], defaults: '0vh'}
-    //           ],
-    //         },{
-    //           property: 'padding',
-    //           properties:[
-    //             { name: 'Top', property: 'padding-top', units: ['vh'], defaults: '0vh'},
-    //             { name: 'Right', property: 'padding-right', units: ['vw'], defaults: '0vw'},
-    //             { name: 'Bottom', property: 'padding-bottom', units: ['vh'], defaults: '0vh'},
-    //             { name: 'Left', property: 'padding-left', units: ['vw'], defaults: '0vw'}
-    //           ],
-    //         }],
-    //       },{
-    //         name: 'Typography',
-    //         open: false,
-    //         buildProps: ['font-family', 'font-size', 'font-weight', 'letter-spacing', 'color', 'line-height', 'text-align', 'text-decoration', 'font-style', 'vertical-align', 'text-shadow'],
-    //         properties:[
-    //           { name: 'Font', property: 'font-family'},
-    //           { name: 'Weight', property: 'font-weight'},
-    //           { name: 'Font color', property: 'color'},
-    //           {
-    //             property: 'text-align',
-    //             type: 'radio',
-    //             defaults: 'left',
-    //             list: [
-    //               { value: 'left', name: 'Left', className: 'fa fa-align-left'},
-    //               { value: 'center', name: 'Center', className: 'fa fa-align-center' },
-    //               { value: 'right', name: 'Right', className: 'fa fa-align-right'},
-    //               { value: 'justify', name: 'Justify', className: 'fa fa-align-justify'}
-    //             ],
-    //           },{
-    //             property: 'text-decoration',
-    //             type: 'radio',
-    //             defaults: 'none',
-    //             list: [
-    //               { value: 'none', name: 'None', className: 'fa fa-times'},
-    //               { value: 'underline', name: 'underline', className: 'fa fa-underline' },
-    //               { value: 'line-through', name: 'Line-through', className: 'fa fa-strikethrough'}
-    //             ],
-    //           },{
-    //             property: 'font-style',
-    //             type: 'radio',
-    //             defaults: 'normal',
-    //             list: [
-    //               { value: 'normal', name: 'Normal', className: 'fa fa-font'},
-    //               { value: 'italic', name: 'Italic', className: 'fa fa-italic'}
-    //             ],
-    //           },{
-    //             property: 'vertical-align',
-    //             type: 'select',
-    //             defaults: 'baseline',
-    //             list: [
-    //               { value: 'baseline'},
-    //               { value: 'top'},
-    //               { value: 'middle'},
-    //               { value: 'bottom'}
-    //             ],
-    //           },{
-    //             property: 'text-shadow',
-    //             properties: [
-    //               { name: 'X position', property: 'text-shadow-h'},
-    //               { name: 'Y position', property: 'text-shadow-v'},
-    //               { name: 'Blur', property: 'text-shadow-blur'},
-    //               { name: 'Color', property: 'text-shadow-color'}
-    //             ],
-    //         }],
-    //       },{
-    //         name: 'Decorations',
-    //         open: false,
-    //         buildProps: ['background-color', 'border-collapse', 'border-radius', 'border', 'background'],
-    //         properties: [{
-    //           property: 'background-color',
-    //           name: 'Background',
-    //         },{
-    //           property: 'border-radius',
-    //           properties  : [
-    //             { name: 'Top', property: 'border-top-left-radius'},
-    //             { name: 'Right', property: 'border-top-right-radius'},
-    //             { name: 'Bottom', property: 'border-bottom-left-radius'},
-    //             { name: 'Left', property: 'border-bottom-right-radius'}
-    //           ],
-    //         },{
-    //           property: 'border-collapse',
-    //           type: 'radio',
-    //           defaults: 'separate',
-    //           list: [
-    //             { value: 'separate', name: 'No'},
-    //             { value: 'collapse', name: 'Yes'}
-    //           ],
-    //         },
-    //         {
-    //           property: 'border',
-    //           properties: [
-    //             { name: 'Width', property: 'border-width', defaults: '0'},
-    //             { name: 'Style', property: 'border-style'},
-    //             { name: 'Color', property: 'border-color'},
-    //           ],
-    //         },{
-    //           property: 'background',
-    //           properties: [
-    //             { name: 'Image', property: 'background-image'},
-    //             { name: 'Repeat', property:   'background-repeat'},
-    //             { name: 'Position', property: 'background-position'},
-    //             { name: 'Attachment', property: 'background-attachment'},
-    //             { name: 'Size', property: 'background-size'}
-    //           ],
-    //         }],
-    //     }];
+        if (opts.updateStyleManager) {
+            const styleManagerSectors = [{
+                name: 'Dimension',
+                id: 'dimension',
+                properties: [{
+                    property: 'width',
+                    units: ['vw'],
+                    defaults: '10vw',
+                }, {
+                    property: 'height',
+                    units: ['vh'],
+                    defaults: '10vh',
+                }, {
+                    property: 'margin',
+                    properties: [
+                        { name: 'Top', property: 'margin-top', units: ['vh'], defaults: '0vh' },
+                        { name: 'Right', property: 'margin-right', units: ['vw'], defaults: '0vw' },
+                        { name: 'Bottom', property: 'margin-bottom', units: ['vh'], defaults: '0vh' },
+                        { name: 'Left', property: 'margin-left', units: ['vw'], defaults: '0vw' }
+                    ],
+                }, {
+                    property: 'padding',
+                    properties: [
+                        { name: 'Top', property: 'padding-top', units: ['vh'], defaults: '0vh' },
+                        { name: 'Right', property: 'padding-right', units: ['vw'], defaults: '0vw' },
+                        { name: 'Bottom', property: 'padding-bottom', units: ['vh'], defaults: '0vh' },
+                        { name: 'Left', property: 'padding-left', units: ['vw'], defaults: '0vw' }
+                    ],
+                }]
+            }, {
+                name: 'Position',
+                id: 'position',
+                properties: [{
+                    property: 'position',
+                    type: 'select',
+                    defaults: 'relative',
+                    options: [
+                        { id: 'relative', label: 'Relative' },
+                        { id: 'absolute', label: 'Free Position' }
+                    ]
+                }, {
+                    property: 'left',
+                    units: ['vw'],
+                    defaults: '50vw'
+                }, {
+                    property: 'top',
+                    units: ['vh'],
+                    defaults: '50vh'
+                }, {
+                    property: 'transform',
+                    defaults: 'translate(-50%, -50%)'
+                }]
+            }, {
+                name: 'Alignment',
+                id: 'alignment',
+                properties: [{
+                    property: 'display',
+                    type: 'select',
+                    defaults: 'flex',
+                    options: [
+                        { id: 'flex', label: 'Flex' }
+                    ]
+                }, {
+                    property: 'justify-content',
+                    type: 'select',
+                    defaults: 'center',
+                    options: [
+                        { id: 'center', label: 'Center' },
+                        { id: 'flex-start', label: 'Start' },
+                        { id: 'flex-end', label: 'End' },
+                        { id: 'space-between', label: 'Space Between' },
+                        { id: 'space-around', label: 'Space Around' },
+                        { id: 'space-evenly', label: 'Space Evenly' }
+                    ]
+                }, {
+                    property: 'align-items',
+                    type: 'select',
+                    defaults: 'center',
+                    options: [
+                        { id: 'center', label: 'Center' },
+                        { id: 'flex-start', label: 'Start' },
+                        { id: 'flex-end', label: 'End' },
+                        { id: 'space-between', label: 'Space Between' },
+                        { id: 'space-around', label: 'Space Around' },
+                        { id: 'space-evenly', label: 'Space Evenly' }
+                    ]
+                }]
+            }];
 
-    // //     // Add our custom sectors
-    //     styleManagerSectors.forEach(sector => 
-    //         editor.StyleManager.addSector(sector.name, sector)
-    //     );
-    // }
+            // Add sectors properly
+            styleManagerSectors.forEach(sector => 
+                editor.StyleManager.addSector(sector.id, sector)
+            );
+        }
+    });
 }
