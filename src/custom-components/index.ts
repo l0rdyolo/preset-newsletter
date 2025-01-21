aefaa9eb9949db5c41a97d64bb28ecaf1bc256a4
Omer <62906173+l0rdyolo@users.noreply.github.com>
2025-01-22 01:04:23 +0300

    button added

--- src/custom-components/index.ts
+++ src/custom-components/index.ts
@@ -1,6 +1,8 @@
 import type { Editor } from 'grapesjs';
 import Image from './Image';
+import Button from './Button';
 
 export default function initializeComponents(editor: Editor) {
     Image(editor);
+    Button(editor);
 } 
